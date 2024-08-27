7d8758c312b709e2ab0b36ab4257d70032ea658b
Ismael P <iperez08@gmail.com>
2024-08-27 18:13:28 -0400

    startup code

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import {Routes, Route} from "react-router-dom"
+import NavBar from './components/NavBar/NavBar'
+import './App.css'
+
+function App() {
+  const [user, setUser] = useState(null)
+  return (
+    <>
+    <NavBar user={user}/>
+    </>
+  )
+}
+
+export default App
