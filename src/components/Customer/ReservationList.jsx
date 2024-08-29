be20ffdd98cb8076a56cee3f57fb68ecbbe7bd3d
Ismael P. <iperez08@gmail.com>
2024-08-29 16:22:24 -0400

    code edit button for customer reservations

--- src/components/Customer/ReservationList.jsx
+++ src/components/Customer/ReservationList.jsx
@@ -1,12 +1,12 @@
 import ReservationCard from "./ReservationCard"
 
 function ReservationList(props) {
-  const { reservations } = props
+  const { reservations, helpers } = props
 
   return (
     <div className="grid-container">
       {reservations.map((reservation) => {
-        return <ReservationCard location={reservation.location} guests={reservation.guests} date={reservation.date} time={reservation.time} key={reservation.location + reservation.guests + reservation.time} />
+        return <ReservationCard location={reservation.location} guests={reservation.guests} date={reservation.date} time={reservation.time} key={reservation.location + reservation.guests + reservation.time} helpers={helpers}/>
       })}
     </div>
   )
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -4,11 +4,12 @@ import LandingPage from "./LandingPage"
 
 // Home component separates by user: guest, customer, or restaurant
 // Customer and Restaurant have their own components while all guests go to LandingPage component
+
 function Home({ user, restaurants, methods }) {
     return (
         <>
         {user ? 
-        user.name ? <RestaurantDashboard restaurant={user}/> : <CustomerDashboard restaurants={restaurants} reservations={reservations} searchRestaurants={methods.searchRestaurants}/>
+        user.name ? <RestaurantDashboard restaurant={user}/> : <CustomerDashboard restaurants={restaurants} user={user} searchRestaurants={methods.searchRestaurants}/>
         :
         <LandingPage restaurants={restaurants} />
         }
