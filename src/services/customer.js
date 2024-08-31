fb6469fd8532ab706b86a2c6515106461fada5a6
Ismael P. <iperez08@gmail.com>
2024-08-31 12:00:19 -0400

    customer signup and signin services

--- src/services/customer.js
+++ src/services/customer.js
@@ -1,59 +1,6 @@
 const BASE_URL = import.meta.env.VITE_NODE_ENV === 'production' ? import.meta.env.VITE_PRODUCTION : import.meta.env.VITE_DEVELOPMENT
 
-export const customerSignUp = async (formData) => {
-    try {
-        const response = await fetch(`${BASE_URL}/customers/signup`,
-            {
-                method: 'POST',
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(formData)
-            }
-        )
 
-        const json = await response.json()
-
-        if (json.error) {
-            throw new Error(json.error)
-        }
-
-        if (json.token) {
-            localStorage.setItem('token', json.token)
-            return json
-        }
-
-    } catch (error) {
-        console.error(error)
-        throw error
-    }
-}
-
-export const customerSignIn = async (formData) => {
-    try {
-        const response = await fetch(`${BASE_URL}/customers/signin`, {
-            method: 'POST',
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(formData)
-        })
-
-        const json = await response.json()
-
-        if (json.error) {
-            throw new Error(json.error)
-        }
-
-        if (json.token) {
-            localStorage.setItem('token', json.token)
-
-            const user = JSON.parse(atob(json.token.split(".")[1]))
-
-            return user
-        }
-
-    } catch (error) {
-        console.error(error)
-        throw error
-    }
-} 
 
 export const getUser = () => {
     const token = localStorage.getItem("token")
